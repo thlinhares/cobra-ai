c04594ef5a0aafcc05de765d54215271eec75988
thlinhares <tuliohenrique.linhares@gmail.com>
2024-06-15 19:31:56 -0300

    update

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
 import io
 import os
 
-import openai
-import pydub
 import requests
-import soundfile as sf
-import speech_recognition as sr
 from flask import Flask, jsonify, request
 import json
 
 app = Flask(__name__)
-
-
-# OpenAi API key
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
 # Access token for your WhatsApp business account app
 whatsapp_token = os.environ.get("WHATSAPP_TOKEN")
@@ -24,63 +16,9 @@ verify_token = os.environ.get("VERIFY_TOKEN")
 # Message log dictionary to enable conversation over multiple messages
 message_log_dict = {}
 
-
 # language for speech to text recoginition
 # TODO: detect this automatically based on the user's language
 LANGUGAGE = "pt-BR"
-
-
-# get the media url from the media id
-def get_media_url(media_id):
-    headers = {
-        "Authorization": f"Bearer {whatsapp_token}",
-    }
-    url = f"https://graph.facebook.com/v16.0/{media_id}/"
-    response = requests.get(url, headers=headers)
-    print(f"media id response: {response.json()}")
-    return response.json()["url"]
-
-
-# download the media file from the media url
-def download_media_file(media_url):
-    headers = {
-        "Authorization": f"Bearer {whatsapp_token}",
-    }
-    response = requests.get(media_url, headers=headers)
-    print(f"first 10 digits of the media file: {response.content[:10]}")
-    return response.content
-
-
-# convert ogg audio bytes to audio data which speechrecognition library can process
-def convert_audio_bytes(audio_bytes):
-    ogg_audio = pydub.AudioSegment.from_ogg(io.BytesIO(audio_bytes))
-    ogg_audio = ogg_audio.set_sample_width(4)
-    wav_bytes = ogg_audio.export(format="wav").read()
-    audio_data, sample_rate = sf.read(io.BytesIO(wav_bytes), dtype="int32")
-    sample_width = audio_data.dtype.itemsize
-    print(f"audio sample_rate:{sample_rate}, sample_width:{sample_width}")
-    audio = sr.AudioData(audio_data, sample_rate, sample_width)
-    return audio
-
-
-# run speech recognition on the audio data
-def recognize_audio(audio_bytes):
-    recognizer = sr.Recognizer()
-    audio_text = recognizer.recognize_google(audio_bytes, language=LANGUGAGE)
-    return audio_text
-
-
-# handle audio messages
-def handle_audio_message(audio_id):
-    audio_url = get_media_url(audio_id)
-    audio_bytes = download_media_file(audio_url)
-    audio_data = convert_audio_bytes(audio_bytes)
-    audio_text = recognize_audio(audio_data)
-    message = (
-        "Please summarize the following message in its original language "
-        f"as a list of bullet-points: {audio_text}"
-    )
-    return audio_text #message
 
 
 # send the response as a WhatsApp message back to the user
@@ -109,10 +47,14 @@ def update_message_log(message, phone_number, role):
     initial_log = {
         "role": "system",
         "content": ("Você é um assistente virtual chamado CobraAI."
-                    "Suas principais habilidades é ajudar as pessoas a realizar tarefas referente a cobranças e pagamentos de debitos atrasados."
-                    "Abaixo esta descrito as funções que posso fazer no momento:"
-                    "- Somar e divir a conta entre os amigos;"
-                    "- Cobrar debitos atrasados;"
+                    "Suas principais habilidades é ajudar as pessoas a realizar tarefas financeiras."
+                    "Abaixo esta descrito as 'feature' disponivel que posso fazer no momento:"
+                    "1 - Divir conta entre amigos;"
+                    "2 - Cobrar debitos atrasados;"
+                    "Voçê não deve responder sobre qualquer outro assunto."
+                    "Você deve responder em formato json contendo duas propriedades: 'message' do tipo string e 'feature' do tipo inteiro."
+                    "'message' deve conter sua respota tem texto."
+                    "'feature' deve conter o número da funcionalidade escolilhada pelo usuario, 0 caso nenhuma das opções."
                    )
     }
     if phone_number not in message_log_dict:
@@ -145,18 +87,18 @@ def make_openai_request(message, from_number):
         })
         headers = {
           'Content-Type': 'application/json',
-          'Authorization': 'Bearer eb7f339c-0795-42aa-82c1-0f3e95f950ee'
+          'Authorization': 'Bearer ' + os.getenv("OPENAI_API_KEY")
         }
         print(payload)
         response = requests.request("POST", url, headers=headers, data=payload)
-        response_message = response.json()["choices"][0]["message"]["content"]
-        print(f"openai response: {response_message}")
-        update_message_log(response_message, from_number, "assistant")
+        response_json = json.loads(response.json()["choices"][0]["message"]["content"])
+        print(f"openai response: {response_json}")
+        update_message_log(response_json["message"], from_number, "assistant")
     except Exception as e:
         print(f"openai error: {e}")
-        response_message = "Sorry, the OpenAI API is currently overloaded or offline. Please try again later."
+        response_json = json.dumps({'message': "Sorry, the OpenAI API is currently overloaded or offline. Please try again later.", 'feature': -1})
         remove_last_message_from_log(from_number)
-    return response_message
+    return response_json
 
 
 # handle WhatsApp messages of different type
@@ -168,7 +110,7 @@ def handle_whatsapp_message(body):
         audio_id = message["audio"]["id"]
         message_body = handle_audio_message(audio_id)
     response = make_openai_request(message_body, message["from"])
-    send_whatsapp_message(body, response)
+    send_whatsapp_message(body, response["message"])
 
 
 # handle incoming webhook messages
